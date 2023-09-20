d639cea30acb7bd7781a1ecc8df3a077ac36dda6
Eunice Mutope <120035932+eunicemtp@users.noreply.github.com>
2023-09-20 11:45:16 +0200

    gsap

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,9 @@
 'use strict'
 
 import { gsap } from 'gsap';
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
+import { MotionPathPlugin } from "gsap/dist/MotionPathPlugin";
+import { PixiPlugin } from "gsap/dist/PixiPlugin";
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, MotionPathPlugin, PixiPlugin);
